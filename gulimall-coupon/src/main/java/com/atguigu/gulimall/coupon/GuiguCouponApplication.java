2a5434b4fd35605f292b564ed1cc4d39774cfcb2
jiajia <jiajia@bilibili.com>
2022-08-21 12:12:15 +0800

    initial commit

---
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GuiguCouponApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiguCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiguCouponApplication.class, args);
+	}
+
+}
